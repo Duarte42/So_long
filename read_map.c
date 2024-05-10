e2a8d94eb59d63f7103c080961691162db64e150
Duarte42 <149487146+Duarte42@users.noreply.github.com>
2024-05-10 15:32:48 +0100

    Add files via upload

---
+++ read_map.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_map.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: duamarqu <duamarqu@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/09 11:12:02 by duamarqu          #+#    #+#             */
+/*   Updated: 2024/05/10 15:28:24 by duamarqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/so_long.h"
+
+static int	strlen_helper(const char *s1)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s1[i] != '\n')
+		i++;
+	return (i);
+}
+
+int	read_map(t_struct *game, char *map)
+{
+	char	*str;
+
+	game->fd = open(map, O_RDONLY, 0);
+	game->map_height = 0;
+	str = get_next_line(game->fd);
+	if (!str)
+		return (printf("no map\n") * 0);
+	game->map_width = strlen_helper(str);
+	while (str)
+	{
+		free(str);
+		str = get_next_line(game->fd);
+		if (str && game->map_width != strlen_helper(str))
+		{
+			free(str);
+			printf("Map is not retangle\n");
+			close(game->fd);
+			return (0);
+		}
+		game->map_height++;
+	}
+	close(game->fd);
+	free(str);
+	return (1);
+}
